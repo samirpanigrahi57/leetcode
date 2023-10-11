d4d193961575353876c39f046d76e377c1d5b58c
Samir Panigrahi <samirpanigrahhi57@gmail.com>
2023-10-11 10:51:38 -0500

    remove duplicates from sorted array

--- src/topInterviewQuestions/easy/RemoveDuplicates.java
+++ src/topInterviewQuestions/easy/RemoveDuplicates.java
@@ -3,5 +3,39 @@ package topInterviewQuestions.easy;
 public class RemoveDuplicates {
     public static void main(String[] args) {
 
+       removeDuplicates(new int[]{1,1,2,2,2,3,4,5});
+        System.out.println(removeDups(new int[]{1,1,2,2,2,3,3,4,5}));
+
+    }
+
+    public static int removeDups(int[] nums){
+        int uniqueIndex = 1;
+        for(int i = 1; i < nums.length; i ++){
+            if(nums[i-1] != nums[i]){
+                nums[uniqueIndex] = nums[i];
+                uniqueIndex++;
+            }
+        }
+
+        return uniqueIndex;
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int visitedElement = nums[0];
+        int[] res = new int[nums.length];
+        int counter = 0;
+        int duplicates = 0;
+        res[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(visitedElement != nums[i]){
+                counter ++;
+                res[counter] = nums[i];
+                visitedElement = nums[i];
+            }else{
+                duplicates ++;
+            }
+        }
+        nums = res;
+        return counter+1;
     }
 }
