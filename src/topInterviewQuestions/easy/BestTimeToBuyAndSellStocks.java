b381e8976e18b51fc0995d9127d252984789f78b
Samir Panigrahi <samirpanigrahhi57@gmail.com>
2023-10-11 11:37:41 -0500

    program: best time to buy and sell stocks

---
+++ src/topInterviewQuestions/easy/BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,19 @@
+package topInterviewQuestions.easy;
+
+public class BestTimeToBuyAndSellStocks {
+    public static void main(String[] args) {
+        System.out.println(buyAndSell(new int[]{7,1,5,3,6,4}));
+        System.out.println(buyAndSell(new int[]{1,2,3,4,5}));
+    }
+    private static int buyAndSell(int[] prices){
+        int profit = 0;
+
+        for(int i = 1; i < prices.length; i++){
+          if(prices[i] > prices[i-1]){
+              profit += prices[i] - prices[i-1];
+          }
+        }
+
+        return profit;
+    }
+}
