aa0a704ad08b8ffc3d78f3078715e8da5ab21f81
Samir Panigrahi <samirpanigrahhi57@gmail.com>
2023-11-01 15:05:42 -0500

    refactor

--- src/topInterviewQuestions/easy/linkedlist/ReverseLinkedList.java
+++ src/topInterviewQuestions/easy/linkedlist/ReverseLinkedList.java
@@ -6,22 +6,6 @@ import topInterviewQuestions.utils.ListNode;
 //https://www.youtube.com/watch?v=ugQ2DVJJroc
 public class ReverseLinkedList {
 
-    /*public static class ListNode {
-        public int val;
-        public ListNode next;
-
-        public ListNode() {
-        }
-
-        public ListNode(int val) {
-            this.val = val;
-        }
-
-        public ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }*/
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
