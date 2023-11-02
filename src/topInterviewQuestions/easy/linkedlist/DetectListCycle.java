c667e2a554e9f2fbcd9d56ec50bf31e1fde35def
Samir Panigrahi <samirpanigrahhi57@gmail.com>
2023-11-02 16:38:51 -0500

    program: DetectListCycle

---
+++ src/topInterviewQuestions/easy/linkedlist/DetectListCycle.java
@@ -0,0 +1,33 @@
+package topInterviewQuestions.easy.linkedlist;
+
+public class DetectListCycle {
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static boolean detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
