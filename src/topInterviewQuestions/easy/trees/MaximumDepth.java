4bf7ef92e8822e40b0578b2c8a1cb6202fb10c0b
Samir Panigrahi <samirpanigrahhi57@gmail.com>
2023-11-02 16:52:56 -0500

    refactor

--- src/topInterviewQuestions/easy/trees/MaximumDepth.java
+++ src/topInterviewQuestions/easy/trees/MaximumDepth.java
@@ -1,29 +1,12 @@
 package topInterviewQuestions.easy.trees;
 
+import topInterviewQuestions.utils.TreeNode;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 
 public class MaximumDepth {
 
-    static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 
     public static void main(String[] args) {
         TreeNode node = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
---
+++ src/topInterviewQuestions/utils/TreeNode.java
@@ -0,0 +1,22 @@
+package topInterviewQuestions.utils;
+
+import topInterviewQuestions.easy.trees.MaximumDepth;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
